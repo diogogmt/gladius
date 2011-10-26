b5903b6bc7407e29c6b621ab8840a2a6d6346de9
Diogo Golovanevsky Monteiro <diogo.gmt@gmail.com>
2011-10-26 19:36:55 -0400

    Cache module with example and qunit

---
+++ test/core/Cache.js
@@ -0,0 +1,38 @@
+/*jshint white: false, strict: false, plusplus: false, onevar: false,
+  nomen: false */
+/*global gladius: false, document: false, window: false, module: false, start,
+  test: false, expect: false, ok: false, notEqual: false, stop, QUnit: false */
+
+(function() {
+
+    var engine = null;
+
+    module( 'core/Cache', {
+        setup: function () {
+            stop();
+
+            gladius.create( { debug: true }, function( instance ) {       
+                engine = instance;
+                start();
+            });
+        },
+
+        teardown: function () {
+            engine = null;
+        }
+    });
+
+    test( 'Construction', function () {
+        expect( 1 );
+
+        var cache = new engine.core.Cache();
+        
+        ok(
+                cache,
+                'New cache is constructed.'
+        );
+    });
+
+
+
+}());
